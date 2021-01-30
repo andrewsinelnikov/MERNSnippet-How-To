70cdd0294dc477d6ef8b47909595235c4e5142e0
Andrew Sinelnikov <50057697+andrewsinelnikov@users.noreply.github.com>
2021-01-30 11:22:16 +0200

    Update Burger.js

--- task4/src/components/Navbar/Burger.js
+++ task4/src/components/Navbar/Burger.js
@@ -1,20 +1,9 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 
 import s from "./Navbar.module.css";
 
 const Burger = ({ open, setOpen }) => {
-    useEffect(() => {
-        if (open) {
-          document.body.style.overflow = 'hidden';
-          document.body.style.paddingRight = '15px';
-        }
-        return () => {
-          document.body.style.overflow = 'unset';
-          document.body.style.paddingRight = '0px';
-        };
-      }, [open]);
-
     return (
         <div open={open} onClick={() => setOpen(!open)} className={s.nav__burger}>
                 {open ? <AiOutlineClose /> : <AiOutlineMenu />}
