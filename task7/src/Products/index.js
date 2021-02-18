fc2c6f1cc711669e6ab1517b76762de4a8924286
Andrew Sinelnikov <50057697+andrewsinelnikov@users.noreply.github.com>
2021-02-18 10:57:42 +0200

    Add files via upload

--- task7/src/Products/index.js
+++ task7/src/Products/index.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import ProductItem from './ProductItem';
 
+import s from "./Products.module.css";
+
+const Products = () => {
+    return (
+        <div className={s.products}>
+            <ProductItem img='../../img/E_1.jpg' title='Navarra Ash' price={7}/>
+        </div>
+    )
+}
+
+export default Products
