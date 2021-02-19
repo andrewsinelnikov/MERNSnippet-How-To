5f736aa8dffb940b9636a5b59efa567388cdcc16
Andrew Sinelnikov <50057697+andrewsinelnikov@users.noreply.github.com>
2021-02-19 11:11:49 +0200

    Add files via upload

--- task8/src/Products/ProductIem/index.js
+++ task8/src/Products/ProductIem/index.js
@@ -1,1 +1,21 @@
+import React from 'react';
 
+import s from "./ProductItem.module.css";
+
+const ProductItem = ({ img, title, price }) => {
+    return (
+        <div className={s.card}>
+            <div className={s.card__top}>
+                <a href='#!'>
+                    <img src={ img } alt='product' className={s.card__img}></img>
+                </a>
+            </div>
+           <div className={s.card__footer}>
+             <p>{ title }</p>
+             <h5><span>$</span>{ price }</h5>
+           </div>
+        </div>
+    )
+}
+
+export default ProductItem
