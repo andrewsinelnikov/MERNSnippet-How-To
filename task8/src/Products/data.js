4ebdde9e4d44d24ab5dc515525544e1494a29d54
Andrew Sinelnikov <50057697+andrewsinelnikov@users.noreply.github.com>
2021-02-19 11:10:58 +0200

    Add files via upload

---
+++ task8/src/Products/data.js
@@ -0,0 +1,62 @@
+export const storeProducts = [
+    {
+        id: 1,
+        img: '../../img/E_1.jpg',
+        title: 'Navarra Ash', 
+        price: 7,
+    },
+    {
+        id: 2,
+        img: "../../img/E_2.jpg",
+        title: "Sand Orleans Oak",
+        price: 10,
+    },
+    {
+        id: 3,
+        img: "../../img/E_3.jpg",
+        title: "Locarno Cherry",
+        price: 8,
+    },
+    {
+        id: 4,
+        img: "../../img/E_4.jpg",
+        title: "Black Sparkle Grain",
+        price: 15,
+    },
+    {
+        id: 5,
+        img: "../../img/E_5.jpg",
+        title: "Yellow",
+        price: 7,
+    },
+    {
+        id: 6,
+        img: "../../img/E_6.jpg",
+        title: "China Red",
+        price: 6,
+    },
+    {
+        id: 7,
+        img: "../../img/E_7.jpg",
+        title: "Delft Blue",
+        price: 7,
+    },
+    {
+        id: 8,
+        img: "../../img/KS_1.jpg",
+        title: "Loredo Pine",
+        price: 15,
+    },
+    {
+        id: 9,
+        img: "../../img/KS_2.jpg",
+        title: "Redwood",
+        price: 19,
+    },
+    {
+        id: 10,
+        img: "../../img/KS_3.jpg",
+        title: "Pear Red",
+        price: 16,
+    },
+];
--- task8/src/Products/index.js
+++ task8/src/Products/index.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import ProductItem from './ProductItem';
+import { storeProducts } from './data';
 
+import s from "./Products.module.css";
+
+const Products = () => {
+    return (
+        <div className={s.products}>
+            {storeProducts.map((product) => 
+                <ProductItem key={product.key} img={product.img} title={product.title} price={product.price} />
+            )}
+        </div>
+    )
+}
+
+export default Products
