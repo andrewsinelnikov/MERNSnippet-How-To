05edfff6e8ae1c6126d60d23bf4529b1a66e88da
Andrew Sinelnikov <50057697+andrewsinelnikov@users.noreply.github.com>
2021-03-03 18:06:38 +0200

    Add files via upload

--- task9/src/App.js
+++ task9/src/App.js
@@ -1,1 +1,25 @@
+import './App.css';
 
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Contact from './pages/Contact';
+import Cart from './pages/Cart';
+import Error from './pages/Error';
+
+import { Route, Switch } from "react-router-dom";
+import Navbar from './components/Navbar';
+
+function App() {
+  return <>
+    <Navbar />
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/products' component={Products} />
+      <Route exact path='/contact' component={Contact} />
+      <Route exact path='/cart' component={Cart} />
+      <Route component={Error} />
+    </Switch>
+  </>; 
+}
+
+export default App;
