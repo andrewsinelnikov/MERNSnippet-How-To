6bf311161a84de6c18b8b2e3b6238b444b116a55
Andrew Sinelnikov <50057697+andrewsinelnikov@users.noreply.github.com>
2021-03-09 11:15:22 +0200

    Add files via upload

--- task10/src/Contact/index.js
+++ task10/src/Contact/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ContactForm from './ContactForm';
 import ContactMap from './ContactMap';
+import Title from '../../components/Title';
 
 import s from "./Contact.module.css";
 
 const Contact = () => {
     return (
         <div className={s.contact}>
-            <ContactForm className={s.contact__form}/>
-            <ContactMap className={s.contact__map}/>
+            <Title info='contact' highlighted='us'/>
+            <div className={s.contact__info}>
+                <ContactForm className={s.contact__form}/>
+                <ContactMap className={s.contact__map}/>
+            </div>
         </div>
     )
 }
