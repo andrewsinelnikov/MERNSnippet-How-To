adfa4371b42cb75ed2a3b5f3d03c34a25484c4e2
Andrew Sinelnikov <50057697+andrewsinelnikov@users.noreply.github.com>
2021-03-09 11:14:17 +0200

    Add files via upload

--- task8/src/Products/index.js
+++ task8/src/Products/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ProductItem from './ProductItem';
 import { storeProducts } from './data';
+import Title from '../../components/Title';
 
 import s from "./Products.module.css";
 
 const Products = () => {
     return (
         <div className={s.products}>
-            {storeProducts.map((product) => 
-                <ProductItem key={product.key} img={product.img} title={product.title} price={product.price} />
-            )}
+            <Title info='our' highlighted='products'/>
+            <div className={s.products__info}>
+                {storeProducts.map((product) => 
+                    <ProductItem key={product.key} img={product.img} title={product.title} price={product.price} />
+                )}
+            </div>
         </div>
     )
 }
