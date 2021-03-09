143823921f0a840cfed14db78c5648fc5647af3e
Andrew Sinelnikov <50057697+andrewsinelnikov@users.noreply.github.com>
2021-03-09 11:11:20 +0200

    Add files via upload

--- task11/src/pages/Cart/index.js
+++ task11/src/pages/Cart/index.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import Title from '../../components/Title';
+
+import s from "./Cart.module.css";
+
+const Cart = () => {
+    return (
+        <div className={s.cart}>
+            <Title info='your' highlighted='cart'/>
+            Hello from Cart
+        </div>
+    )
+}
+
+export default Cart
 
