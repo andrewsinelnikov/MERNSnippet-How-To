90945ee1aeaedcfb45fa49823171145f828efe20
Andrew Sinelnikov <50057697+andrewsinelnikov@users.noreply.github.com>
2021-03-10 10:19:40 +0200

    Add files via upload

--- task12/src/pages/Error/index.js
+++ task12/src/pages/Error/index.js
@@ -1,1 +1,21 @@
+import React from 'react';
+import Title from '../../components/Title';
+import { Link, useLocation } from "react-router-dom";
 
+import s from "./Error.module.css";
+
+const Error = () => {
+    let location = useLocation();
+
+    return (
+        <div className={s.error}> 
+            <p>You've requested <code>{location.pathname}</code></p>
+            <Title info='the requested page' highlighted='does not exist'/>
+            <Link to="/" className={s.error__redirect}>
+              back to home page
+            </Link>
+        </div>
+    )
+}
+
+export default Error
