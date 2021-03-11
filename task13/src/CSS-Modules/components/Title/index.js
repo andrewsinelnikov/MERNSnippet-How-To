fcf6bea8424fee183d18be812aed422c053eec33
Andrew Sinelnikov <50057697+andrewsinelnikov@users.noreply.github.com>
2021-03-11 10:02:53 +0200

    Add files via upload

--- task13/src/CSS-Modules/components/Title/index.js
+++ task13/src/CSS-Modules/components/Title/index.js
@@ -1,1 +1,13 @@
+import React from 'react';
 
+import s from "./Title.module.css";
+
+const Title = ({info, highlighted}) => {
+    return (
+        <h1 className={s.title__text}>
+            {info} <span className={s.title__highlighted}>{highlighted}</span>
+        </h1>
+    )
+}
+
+export default Title
