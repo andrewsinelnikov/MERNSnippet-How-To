d69a2e67f25cb8de8b07c79c6441ca97875a7c8b
Andrew Sinelnikov <50057697+andrewsinelnikov@users.noreply.github.com>
2021-03-11 10:39:59 +0200

    Add files via upload

---
+++ task13/src/Styled-Components/components/Title/TitleElements.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const TitleContainer = styled.h1`
+    font-size: 2rem;
+    font-weight: bold;
+    text-align: center;
+    text-transform: uppercase;
+    line-height: 1.1;
+    letter-spacing: 0.15rem;
+    color: #333;
+    width: 100%;
+    padding: 0 1rem;
+    margin: 2rem auto;
+
+    .title__highlighted {
+        color: #f77516;
+    }
+`;
--- task13/src/Styled-Components/components/Title/index.js
+++ task13/src/Styled-Components/components/Title/index.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import { TitleContainer } from './TitleElements';
 
+const Title = ({info, highlighted}) => {
+    return (
+        <TitleContainer>
+            {info} <span className='title__highlighted'>{highlighted}</span>
+        </TitleContainer>
+    )
+}
+
+export default Title
