f4ba13a52406a336cfad77c1c54701d72b0f5445
Andrew Sinelnikov <50057697+andrewsinelnikov@users.noreply.github.com>
2021-03-11 09:48:33 +0200

    Add files via upload

--- task13/src/Style-in-App-css/components/Title/index.js
+++ task13/src/Style-in-App-css/components/Title/index.js
@@ -1,1 +1,11 @@
+import React from 'react';
 
+const Title = ({info, highlighted}) => {
+    return (
+        <h1 className='title__text'>
+            {info} <span className='title__highlighted'>{highlighted}</span>
+        </h1>
+    )
+}
+
+export default Title
