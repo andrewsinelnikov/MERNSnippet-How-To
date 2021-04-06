249b0d3a4ad63e48f9ced714ab783970ea7f9f2d
Andrew Sinelnikov <50057697+andrewsinelnikov@users.noreply.github.com>
2021-04-06 11:05:09 +0300

    Add files via upload

--- task16/src/server/server.js
+++ task16/src/server/server.js
@@ -1,1 +1,11 @@
+require('dotenv').config();
+const express = require("express");
+const connectDB = require("./config/db");
 
+connectDB();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
