7968d6347cf5472a72e4053aaff7530eef9f2978
Andrew Sinelnikov <50057697+andrewsinelnikov@users.noreply.github.com>
2021-04-08 11:05:10 +0300

    Add files via upload

--- task17/src/server/models/Product.js
+++ task17/src/server/models/Product.js
@@ -1,1 +1,35 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
+const productSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    info: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    company: {
+        type: String,
+        required: true,
+    },
+    img: {
+        type: String,
+        required: true,
+    },
+    count: {
+        type: Number,
+        required: true,
+    },
+    total: {
+        type: Number,
+        required: true,
+    },
+});
+
+module.exports = mongoose.model("product", productSchema);
