d1929b2fa3bea884aef56abd40a48bc4e448a33f
Andrew Sinelnikov <50057697+andrewsinelnikov@users.noreply.github.com>
2021-04-13 21:00:27 +0300

    Add files via upload

--- task18/src/server/data/products.js
+++ task18/src/server/data/products.js
@@ -1,1 +1,104 @@
+const storeProducts = [
+    {
+      title: "Navarra Ash",
+      img: "http://www.sdyzain.com.ua/img/dsp/egger/decors/wood/H_1232.jpg",
+      price: 7,
+      company: "EGGER",
+      info:
+        "Decor impresses with its clear grain, while the texture adds to the decor a natural look. Also, it convinces through its light sand colour that allows the decor to be integrated into many designs in a fresh and timeless manner.",
+      count: 100,
+      total: 0
+    },
+    {
+      title: "Sand Orleans Oak",
+      img: "http://www.sdyzain.com.ua/img/dsp/egger/decors/wood/H_1394.jpg",
+      price: 10,
+      company: "EGGER",
+      info:
+        "The decor image of the Sand Orleans Oak works nice in subtle applications. The light pore allows combinations with white colors. But this colour nuance also works well in combinations with grey materials and uni colours. ",
+      count: 100,
+      total: 0
+    },
+    {
+      title: "Locarno Cherry",
+      img: "http://www.sdyzain.com.ua/img/dsp/egger/decors/wood/H_1698.jpg",
+      price: 8,
+      company: "EGGER",
+      info:
+        "This decor represents an update in the area of classic cherry. In line with the trend towards nature, this version has a planked appearance with a beautiful colour play.",
+      count: 100,
+      total: 0
+    },
+    {
+      title: "Black Sparkle Grain",
+      img: "http://www.sdyzain.com.ua/img/dsp/egger/decors/fantasy/F_040.jpg",
+      price: 15,
+      company: "EGGER",
+      info:
+        "The decor combines the characteristics of different materials and is therefore suitable for a wide variety of applications. In the colour black, this style looks like dark concrete, but when combined with ST76 Mineral Rough Matt it also looks like a fine granite or terrazzo.",
+      count: 100,
+      total: 0
+    },
+    {
+      title: "Yellow",
+      img: "http://www.sdyzain.com.ua/img/dsp/egger/decors/uni/U_114.jpg",
+      price: 7,
+      company: "EGGER",
+      info:
+        "This color is best suited as accent colour, alongside dark materials and clear white tones. It is one of the basic colour tones thanks to its vibrancy.",
+      count: 100,
+      total: 0
+    },
+    {
+      title: "China Red",
+      img: "http://www.sdyzain.com.ua/img/dsp/egger/decors/uni/U_321.jpg",
+      price: 7,
+      company: "EGGER",
+      info:
+        "This color is best suited as accent colour. With its rich luminosity, it is a basic colour tone that has a particularly intense effect on dark wood or uni colours.",
+      count: 100,
+      total: 0
+    },
+    {
+      title: "Delft Blue",
+      img: "http://www.sdyzain.com.ua/img/dsp/egger/decors/uni/U_525.jpg",
+      price: 7,
+      company: "EGGER",
+      info:
+        "The intense color is best suited as accent colour on light or white woodgrains. It is a basic tone thanks to its vibrancy.",
+      count: 100,
+      total: 0
+    },
+    {
+      title: "Loredo Pine",
+      img: "http://www.sdyzain.com.ua/img/dsp/kronospan/decors/wood/8914.jpg",
+      price: 15,
+      company: "KRONOSPAN",
+      info:
+        "Loredo Pine represents a decor in the area of dark pine.",
+      count: 100,
+      total: 0
+    },
+    {
+      title: "Redwood",
+      img: "http://www.sdyzain.com.ua/img/dsp/kronospan/decors/wood/775.jpg",
+      price: 19,
+      company: "KRONOSPAN",
+      info:
+        "Redwood represents a decor in the area of luxurios.",
+      count: 100,
+      total: 0
+    },
+    {
+      title: "Pear Red",
+      img: "http://www.sdyzain.com.ua/img/dsp/kronospan/decors/wood/1625.jpg",
+      price: 16,
+      company: "KRONOSPAN",
+      info:
+        "Pear Red represents a decor in the area of dark pear.",
+      count: 100,
+      total: 0
+    }
+  ];
 
+  module.exports = storeProducts;
