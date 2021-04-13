82fc890cd9da2f3c687d47b1f58446c23159ed9f
Andrew Sinelnikov <50057697+andrewsinelnikov@users.noreply.github.com>
2021-04-13 20:59:23 +0300

    Add files via upload

--- task18/src/server/dataScript.js
+++ task18/src/server/dataScript.js
@@ -1,1 +1,24 @@
+require('dotenv').config();
 
+const storeProducts = require('./data/products');
+const connectDB = require('./config/db');
+const Product = require('./models/Product');
+
+connectDB();
+
+const importData = async () => {
+    try {
+        await Product.deleteMany({});
+        
+        await Product.insertMany(storeProducts);
+
+        console.log('MongoDB imported your data');
+
+        process.exit();
+    } catch (error) {
+        console.error('Error with data import');
+        process.exit(1);
+    }
+}
+
+importData();
