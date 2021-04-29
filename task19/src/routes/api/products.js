98faf77e11e081c28a347665ca90b086a484e771
Andrew Sinelnikov <50057697+andrewsinelnikov@users.noreply.github.com>
2021-04-29 11:45:03 +0300

    Add files via upload

--- task19/src/routes/api/products.js
+++ task19/src/routes/api/products.js
@@ -1,1 +1,19 @@
+const express = require("express");
+const router = express.Router();
 
+const { 
+    getProducts, 
+    getProductById 
+} = require("../../controllers/products");
+
+// @route    GET /api/products
+// @desc     Get all products
+// @access   Public
+router.get('/', getProducts);
+
+// @route    GET /api/products/:id
+// @desc     Get a product by ID
+// @access   Public
+router.get('/:id', getProductById);
+
+module.exports = router;
