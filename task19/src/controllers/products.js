0d0d83ecb235620f7935d9db9857786a5d2f5b3f
Andrew Sinelnikov <50057697+andrewsinelnikov@users.noreply.github.com>
2021-04-29 11:42:03 +0300

    Add files via upload

--- task19/src/controllers/products.js
+++ task19/src/controllers/products.js
@@ -1,1 +1,28 @@
+const Product = require("../../models/Product");
 
+const getProducts = async (req, res) => {
+    try {
+        const products = await Product.find({});
+
+        res.json(products);
+    } catch (error) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+}
+
+const getProductById = async (req, res) => {
+    try {
+        const products = await Product.findById(req.params.id);
+
+        res.json(products);
+    } catch (error) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+}
+
+module.exports = {
+    getProducts,
+    getProductById,
+}
