d02d2edfb0fb32679ab2397f2d6e8e5723fa88a5
Andrew Sinelnikov <50057697+andrewsinelnikov@users.noreply.github.com>
2021-05-07 09:54:37 +0300

    Add files via upload

--- task20/src/config/db.js
+++ task20/src/config/db.js
@@ -1,1 +1,19 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
 
+const connectDB = async () => {
+    try {
+      await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      });
+  
+      console.log("MongoDB Connected...");
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+  };
+
+  module.exports = connectDB;
