5afacd0a184d87b9716a20e9440738a7939182f8
Andrew Sinelnikov <50057697+andrewsinelnikov@users.noreply.github.com>
2021-06-02 14:51:28 +0300

    Add files via upload

--- task22/src/App.js
+++ task22/src/App.js
@@ -1,1 +1,16 @@
+import React, { Fragment } from 'react';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Fragment>
+        Store
+      </Fragment>
+    </Provider>
+  );
+}
+
+export default App;
