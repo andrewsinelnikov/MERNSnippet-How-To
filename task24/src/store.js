f1384a50e541ba4d897570aea122c6d1e85596fd
Andrew Sinelnikov <50057697+andrewsinelnikov@users.noreply.github.com>
2021-06-15 13:38:41 +0300

    Add files via upload

--- task24/src/store.js
+++ task24/src/store.js
@@ -1,1 +1,16 @@
+import { createStore, applyMiddleware } from 'redux';
+// import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import reducer from './reducers';
 
+// const reducer = combineReducers({});
+
+const middleware = [thunk];
+
+const store = createStore(
+        reducer,
+        composeWithDevTools(applyMiddleware(...middleware))
+    );
+
+export default store;
